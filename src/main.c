4b803fe05a0c42e30ae892861198f552c4b8fd32
ChenFeng-z <896512290@qq.com>
2026-03-13 18:38:44 +0800

    3.09.全屏渲染

--- src/main.c
+++ src/main.c
@@ -18,6 +18,13 @@ bool initialize_window(void){
         fprintf(stderr, "Error initializing SDL.\n");
         return false;
     }
+
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+
+    //window_width = display_mode.w;
+    //window_height = display_mode.h;
+
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
@@ -37,6 +44,8 @@ bool initialize_window(void){
         fprintf(stderr, "Error creating SDL renderer.\n");
         return false;
     }
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
     return true;
 }
 
