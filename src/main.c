508466c41b0d7c03f7343aeef061b3839191b168
ChenFeng-z <896512290@qq.com>
2026-03-13 16:29:31 +0800

    3.01.窗口的实现

---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+bool is_running = false;
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+bool initialize_window(void){
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window){
+        fprintf(stderr, "Eeeor creating SDL window.\n");
+        return false;
+    }
+
+    //Create a SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer){
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+    return true;
+}
+
+int main(void) {
+
+    is_running = initialize_window();
+
+    return 0;
+}
