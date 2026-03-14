eec6adce88942c31a20d884b8a2b6d75327018f7
ChenFeng-z <896512290@qq.com>
2026-03-14 22:31:36 +0800

    4.02.改变了结构，添加了.h文件

--- src/main.c
+++ src/main.c
@@ -1,57 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
+#include "display.h"
 
 bool is_running = false;
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-uint32_t* color_buffer = NULL;
-SDL_Texture* color_buffer_texture = NULL;
-
-int window_width = 800;
-int window_height = 600;
-
-bool initialize_window(void){
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-        fprintf(stderr, "Error initializing SDL.\n");
-        return false;
-    }
-
-    SDL_DisplayMode display_mode;
-    SDL_GetCurrentDisplayMode(0, &display_mode);
-
-    window_width = display_mode.w;
-    window_height = display_mode.h;
-
-    window = SDL_CreateWindow(
-        NULL,
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        window_width,
-        window_height,
-        SDL_WINDOW_BORDERLESS
-    );
-    if (!window){
-        fprintf(stderr, "Eeeor creating SDL window.\n");
-        return false;
-    }
-
-    //Create a SDL renderer
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer){
-        fprintf(stderr, "Error creating SDL renderer.\n");
-        return false;
-    }
-    //SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-    return true;
-}
 
 void setup(void) {
-    color_buffer = malloc(sizeof(uint32_t) * window_width  * window_height);
+    // Allocate the required memory in bytes to hold the color buffer
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 
+    // Creating a SDL texture that is used to display the color buffer
     color_buffer_texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_ARGB8888,
@@ -61,11 +20,11 @@ void setup(void) {
     );
 }
 
-void process_input(void){
+void process_input(void) {
     SDL_Event event;
     SDL_PollEvent(&event);
 
-    switch ((event.type)){
+    switch (event.type) {
         case SDL_QUIT:
             is_running = false;
             break;
@@ -74,53 +33,19 @@ void process_input(void){
                 is_running = false;
             break;
     }
-    
 }
 
-void update(void){
-    
+void update(void) {
+    // TODO:
 }
 
-void render_color_buffer(void){
-    SDL_UpdateTexture(
-        color_buffer_texture,
-        NULL,
-        color_buffer,
-        (int)(window_width * sizeof(uint32_t))
-    );
-    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void clear_color_buffer(uint32_t color){
-    for (int y = 0; y < window_height; y++)
-        for (int x = 0; x < window_width; x ++)
-            color_buffer[(window_width*y) + x] = color;
-}
-
-void draw_grid(void){
-    for (int y = 0; y < window_height; y++)
-        for (int x = 0; x < window_width; x ++)
-            if (x % 10 == 0 || y % 10 == 0)
-                color_buffer[(window_width*y) + x] = 0xFF333333;
-}
-
-void draw_rect(int x, int y, int width, int height, uint32_t color){
-    for (int i = 0; i < width; i++){
-        for (int j = 0; j < height; j++){
-            int current_x = x + i;
-            int current_y = y + j;
-            color_buffer[(window_width * current_y) + current_x] = color;
-        }
-    }
-}
-
-void render(void){
-    SDL_SetRenderDrawColor(renderer,0, 0, 0, 255);
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     draw_grid();
 
-    draw_rect(300, 200, 300, 150, 0xFFF00FF);
+    draw_rect(300, 200, 300, 150, 0xFFFF00FF);
 
     render_color_buffer();
     clear_color_buffer(0xFF000000);
@@ -128,21 +53,12 @@ void render(void){
     SDL_RenderPresent(renderer);
 }
 
-void destroy_window(void){
-    // 反过来销毁
-    free(color_buffer);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-int main(int argc, char* argv[]) {
-
+int main(void) {
     is_running = initialize_window();
 
     setup();
 
-    while(is_running){
+    while (is_running) {
         process_input();
         update();
         render();
