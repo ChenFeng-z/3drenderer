8e37ce219bf9d428d72db98e26e06f22fcdd613f
ChenFeng-z <896512290@qq.com>
2026-03-15 08:51:37 +0800

    5.05.绘制中心在原点，边长为2的立方体

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include "display.h"
 #include "vector.h"
 
+const  int N_POINTS = 9 * 9 * 9;
+vec3_t cube_points[N_POINTS];
+
 bool is_running = false;
 
 void setup(void) {
@@ -19,6 +22,17 @@ void setup(void) {
         window_width,
         window_height
     );
+    int point_count = 0;
+
+
+    for (float x = -1; x <= 1; x += 0.25){
+        for (float y = - 1; y <= 1; y += 0.25){
+            for (float z = -1; z <= 1; z += 0.25){
+                vec3_t new_point = {.x = x, .y = y, .z = z};
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
 }
 
 void process_input(void) {
