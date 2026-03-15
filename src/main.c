f831971e4cf37d61010c493aa2abadaeeff7d426
ChenFeng-z <896512290@qq.com>
2026-03-15 09:28:48 +0800

    6.01.把投影居中，合适的间距

--- src/main.c
+++ src/main.c
@@ -5,8 +5,11 @@
 #include "display.h"
 #include "vector.h"
 
-const  int N_POINTS = 9 * 9 * 9;
+#define N_POINTS  (9 * 9 * 9)
 vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+
+float fov_factor = 128;
 
 bool is_running = false;
 
@@ -50,18 +53,36 @@ void process_input(void) {
     }
 }
 
+vec2_t project(vec3_t point){
+    //投影到xoy面上
+    vec2_t projected_point = {
+        .x = (fov_factor * point.x),
+        .y = (fov_factor * point.y)
+    };
+    return projected_point;
+}
+
 void update(void) {
-    // TODO:
+    for(int i = 0; i < N_POINTS; i++){
+        vec3_t point = cube_points[i];
+        vec2_t projected_point = project(point);
+
+        projected_points[i] = projected_point;
+    }
 }
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    draw_grid();
-
-    draw_pixel(20, 20, 0xFFFFFF00);
-    draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+    
+    for (int i = 0; i < N_POINTS; i++){
+        vec2_t projected_point = projected_points[i];
+        draw_rect(
+            projected_point.x + (window_width / 2.0),
+            projected_point.y + (window_height / 2.0),
+            4,
+            4,
+            0xFFFFFF00
+        );
+    }
 
     render_color_buffer();
     clear_color_buffer(0xFF000000);
