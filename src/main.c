8b3ade1263d40635e99cfab60b18e909e8fe94f9
ChenFeng-z <896512290@qq.com>
2026-03-15 10:07:44 +0800

    6.01.添加了透视，z轴不变

--- src/main.c
+++ src/main.c
@@ -9,7 +9,9 @@
 vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
-float fov_factor = 128;
+vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
+
+float fov_factor = 640;
 
 bool is_running = false;
 
@@ -17,7 +19,7 @@ void setup(void) {
     // Allocate the required memory in bytes to hold the color buffer
     color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 
-    // Creating a SDL texture that is used to display the color buffer
+    // Creating a SDL texture that is used to display the color buffer 
     color_buffer_texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_ARGB8888,
@@ -56,8 +58,8 @@ void process_input(void) {
 vec2_t project(vec3_t point){
     //投影到xoy面上
     vec2_t projected_point = {
-        .x = (fov_factor * point.x),
-        .y = (fov_factor * point.y)
+        .x = (fov_factor * point.x) / point.z,
+        .y = (fov_factor * point.y) / point.z
     };
     return projected_point;
 }
@@ -65,6 +67,8 @@ vec2_t project(vec3_t point){
 void update(void) {
     for(int i = 0; i < N_POINTS; i++){
         vec3_t point = cube_points[i];
+
+        point.z -= camera_position.z;
         vec2_t projected_point = project(point);
 
         projected_points[i] = projected_point;
