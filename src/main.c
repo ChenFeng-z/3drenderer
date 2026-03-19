2f81483e854d2046876644529aa3fb28578783f1
ChenFeng-z <896512290@qq.com>
2026-03-19 10:21:50 +0800

    1501.添加深度

--- src/main.c
+++ src/main.c
@@ -175,18 +175,23 @@ void update(void) {
             projected_point[j].x += (window_width / 2);
             projected_point[j].y += (window_height / 2);
         }
+
+        float avg_depth = (transformed_vertices[0].z + transformed_vertices[1].z + transformed_vertices[2].z) / 3.0;
         triangle_t projected_triangle = {
                 .points = {
                     {projected_point[0].x, projected_point[0].y},
                     {projected_point[1].x, projected_point[1].y},
                     {projected_point[2].x, projected_point[2].y},
                 },
-                .color = mesh_face.color
+                .color = mesh_face.color,
+                .avg_depth = avg_depth
             };
 
         // Save the projected triangle in the array of triangles to render
         array_push(triangles_to_render, projected_triangle);
     }
+
+    
 }
 
 ///////////////////////////////////////////////////////////////////////////////
